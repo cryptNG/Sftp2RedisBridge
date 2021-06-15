003c4a6e69d8e807cb65133aa7e50accd1820dc7
cryptocatz <crypt.ng@gmail.com>
2021-06-15 15:15:11 +0200

    TransportUUID is generated from file attributes now
    This should fix an issue with files having new TUUIDs if they are retrieved, these UUIDs would not not match the UUIDs in the memorystore.
    This is untested as of now

--- Sftp2RedisBridge/Networking/RemoteFile.cs
+++ Sftp2RedisBridge/Networking/RemoteFile.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
 using Renci.SshNet.Sftp;
 
 namespace Sftp2RedisBridge.Networking
@@ -11,9 +13,24 @@ namespace Sftp2RedisBridge.Networking
 
         public RemoteFile(SftpFile sftpFile, byte[] data)
         {
-            if (sftpFile != null) TransactionUid = Guid.NewGuid().ToString();
+            if (sftpFile != null) TransactionUid = CreateTransportHashString(sftpFile.Attributes.LastWriteTimeUtc, sftpFile.FullName);
             FullName = sftpFile.FullName;
             Name = sftpFile.Name;
+        }
+
+        public static string CreateTransportHashString(DateTime timeStamp, string fullFileName)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in CreateTransportHash(timeStamp.ToLongTimeString() + fullFileName))
+                sb.Append(b.ToString("X2")); //X2 means format as hexadecimal string
+
+            return sb.ToString();
+        }
+
+        public static byte[] CreateTransportHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
         }
 
         public string TransactionUid { get; set; }
